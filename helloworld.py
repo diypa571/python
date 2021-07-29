44935d6c217e55244e2dd23a7c80cd78d76d7c69
Diyp <diypa571@gmail.com>
2021-07-29 22:33:13 +0200

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print ("Hello World")
+ #Print is used instead cout for cpp, like echo and print for php.
+ # Good to know... hashtag is used for commenting 
+ # That is it, run the helloworld.py int he terminila...
+ #diypa571@diypa:~/Desktop/python$ python helloworld.p
+ 
